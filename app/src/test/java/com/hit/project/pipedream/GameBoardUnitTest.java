d81e5269f42498392366d3116757f713739b1776
Alon A <adam.alon89@gmail.com>
2018-09-10 17:46:31 +0300

    unit test

--- app/src/test/java/com/hit/project/pipedream/GameBoardUnitTest.java
+++ app/src/test/java/com/hit/project/pipedream/GameBoardUnitTest.java
@@ -1,9 +1,8 @@
 package com.hit.project.pipedream;
 
-import android.graphics.Point;
-
 import com.hit.project.pipedream.logic.GameBoard;
 import com.hit.project.pipedream.logic.Pipe;
+import com.hit.project.pipedream.logic.Point;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -37,6 +36,7 @@ public class GameBoardUnitTest {
     @Test
     public void testFlow()
     {
+        //create path
         boolean result = _board.addPipeToBoard(new Point(0,6), Pipe.PipeType.HORIZONTAL);
         assertTrue("failed to add pipe (0,6)",result);
         result = _board.addPipeToBoard(new Point(1,6), Pipe.PipeType.BOTTOM_LEFT);
@@ -51,11 +51,41 @@ public class GameBoardUnitTest {
         assertTrue("failed to add pipe (2,3)",result);
         result = _board.addPipeToBoard(new Point(3,3), Pipe.PipeType.HORIZONTAL);
         assertTrue("failed to add pipe (3,3)",result);
+        //start flow in pipes
+        _board.startGame(new Point(0,6),Pipe.Directions.RIGHT);
+        try {
+            //wait until end of game
+            Thread.sleep( 120*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void addInvalidPipe()
+    public void testJunctionFlow()
     {
-
+        //create path
+        boolean result = _board.addPipeToBoard(new Point(0,6), Pipe.PipeType.HORIZONTAL);
+        assertTrue("failed to add pipe (0,6)",result);
+        result = _board.addPipeToBoard(new Point(1,6), Pipe.PipeType.BOTTOM_LEFT);
+        assertTrue("failed to add pipe (1,6)",result);
+        result = _board.addPipeToBoard(new Point(1,5), Pipe.PipeType.VERTICAL);
+        assertTrue("failed to add pipe (1,5)",result);
+        result = _board.addPipeToBoard(new Point(1,4), Pipe.PipeType.VERTICAL);
+        assertTrue("failed to add pipe (1,4)",result);
+        result = _board.addPipeToBoard(new Point(1,3), Pipe.PipeType.TOP_RIGHT);
+        assertTrue("failed to add pipe (1,3)",result);
+        result = _board.addPipeToBoard(new Point(2,3), Pipe.PipeType.HORIZONTAL);
+        assertTrue("failed to add pipe (2,3)",result);
+        result = _board.addPipeToBoard(new Point(3,3), Pipe.PipeType.HORIZONTAL);
+        assertTrue("failed to add pipe (3,3)",result);
+        //start flow in pipes
+        _board.startGame(new Point(0,6),Pipe.Directions.RIGHT);
+        try {
+            //wait until end of game
+            Thread.sleep( 15*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
