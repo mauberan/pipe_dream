61beade14ae0ef8e46ad50c66cfc7c37ce7e18aa
Alon A <adam.alon89@gmail.com>
2018-09-24 14:19:26 +0300

    flow time (in milliseconds) for pipe added to level settings

--- app/src/main/java/com/hit/project/pipedream/logic/Level.java
+++ app/src/main/java/com/hit/project/pipedream/logic/Level.java
@@ -3,21 +3,21 @@ package com.hit.project.pipedream.logic;
 public class Level {
 
 
-    public static Level[] Levels = {new Level(5,10,50,LevelDifficulty.EASY),
-            new Level(4,10,50,LevelDifficulty.EASY),
-            new Level(5,10,50,LevelDifficulty.EASY),
-            new Level(6,10,50,LevelDifficulty.EASY),
-            new Level(7,10,50,LevelDifficulty.EASY),
-            new Level(8,10,100,LevelDifficulty.MEDIUM),
-            new Level(8,10,100,LevelDifficulty.MEDIUM),
-            new Level(9,10,100,LevelDifficulty.MEDIUM),
-            new Level(9,10,100,LevelDifficulty.MEDIUM),
-            new Level(10,10,100,LevelDifficulty.MEDIUM),
-            new Level(11,10,120,LevelDifficulty.HARD),
-            new Level(11,10,120,LevelDifficulty.HARD),
-            new Level(12,10,120,LevelDifficulty.HARD),
-            new Level(12,10,120,LevelDifficulty.HARD),
-            new Level(13,10,120,LevelDifficulty.HARD)};
+    public static Level[] Levels = {new Level(5,10,50,LevelDifficulty.EASY,1000),
+            new Level(4,10,50,LevelDifficulty.EASY,1000),
+            new Level(5,10,50,LevelDifficulty.EASY,1000),
+            new Level(6,10,50,LevelDifficulty.EASY,1000),
+            new Level(7,10,50,LevelDifficulty.EASY,1000),
+            new Level(8,10,100,LevelDifficulty.MEDIUM,700),
+            new Level(8,10,100,LevelDifficulty.MEDIUM,700),
+            new Level(9,10,100,LevelDifficulty.MEDIUM,700),
+            new Level(9,10,100,LevelDifficulty.MEDIUM,700),
+            new Level(10,10,100,LevelDifficulty.MEDIUM,700),
+            new Level(11,10,120,LevelDifficulty.HARD,400),
+            new Level(11,10,120,LevelDifficulty.HARD,400),
+            new Level(12,10,120,LevelDifficulty.HARD,400),
+            new Level(12,10,120,LevelDifficulty.HARD,400),
+            new Level(13,10,120,LevelDifficulty.HARD,400)};
 
     public enum LevelDifficulty {
         EASY, MEDIUM, HARD
@@ -27,19 +27,26 @@ public class Level {
     private int _timeBeforeFlow;
     private int _pointsPerPipe;
     private LevelDifficulty _difficulty;
+    private int _flowTimeInPipe;
 
     public Level(int requiredPipeLength, int timeBeforeFlow,  int pointsPerPipe,
-            LevelDifficulty difficulty)
+            LevelDifficulty difficulty,int flowTimeInPipe)
     {
         _requiredPipeLength = requiredPipeLength;
         _timeBeforeFlow = timeBeforeFlow;
         _pointsPerPipe = pointsPerPipe;
         _difficulty = difficulty;
+        _flowTimeInPipe = flowTimeInPipe;
     }
 
     public int getRequiredPipeLength()
     {
         return _requiredPipeLength;
+    }
+
+    public int getFlowTimeInPipe()
+    {
+        return _flowTimeInPipe;
     }
 
     public int getTimeBeforeFlow()
